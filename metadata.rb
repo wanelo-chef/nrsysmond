5db38d386c5682ef3b4e0ce096d4095cdfcb0987
Eric Saxby <sax@livinginthepast.org>
2015-09-10 15:54:01 -0700

    0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dev@wanelo.com'
 license 'MIT'
 description 'Installs/Configures nrsysmond'
 long_description 'Installs/Configures nrsysmond'
-version '0.2.1'
+version '0.2.2'
 
 supports 'smartos'
